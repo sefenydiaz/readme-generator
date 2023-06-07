1bf8eba90f026fc4588647589ebcc57d7c33fdaf
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-07 17:21:03 -0400

    added questions array and empty string functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // TITLE
+    "What is your project title?",
+    // DESCRIPTION
+    "What is your desired description for this project?",
+    // INSTALLATION
+    "What are the installation instructions for this project?",
+    // USAGE
+    "What is the usage information for this project?",
+    // LICENSE (included in other js file?)
+    // CONTRIBUTING
+    "What are the contribution guidelines for this project?",
+    // TESTS
+    "What are the test instructions for this project?",
+    // QUESTIONS
+    "What is your GitHub username?",
+    "What is your email address?",
+
+
+];
+console.log(questions);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+}
+
+// returns an empty string if there is no license
+function noBadge() {
+  return "";
+}
+const noLicenseBadge = noBadge();
+console.log(noLicenseBadge);
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
+
+// returns an empty string if there is no license
+function noLink() {
+  return "";
+}
+const noLicenseLink = noLink();
+console.log(noLicenseLink)
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
+
+// returns an empty string if there is no license
+function noSection() {
+  return "";
+}
+const noLicenseSection = noSection();
+console.log(noLicenseSection)
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description ${data.description}
+  ## Table of Contents ${data.table}
+  ## Installation ${data.installation}
+  ## Usage ${data.usage}
+  ## License ${data.license}
+  ## Contributing ${data.contributing} 
+  ## Tests ${data.tests}
+  ## Questions ${data.questions}
 
 `;
 }
